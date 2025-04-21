6e4818ef14289912821995f00e0a8a0a315ee1ba
Rahul Raj <rahulrajdec65@gmail.com>
2025-04-21 16:24:22 +0530

    Aunthication is done

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { login, register ,logout , check } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 const authRoutes = express.Router();
 
 authRoutes.post("/register" , register)
 
 authRoutes.post("/login" , login)
 
-authRoutes.post("/logout" , logout)
+authRoutes.post("/logout" ,authMiddleware, logout)
 
-authRoutes.post("/check" , check)
+authRoutes.post("/check" , authMiddleware, check)
 
 
 
