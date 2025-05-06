660b353ac4bfde50b128fcb9b49840ec907f16e9
Rahul Raj <rahulrajdec65@gmail.com>
2025-05-06 20:50:47 +0530

    execute code updated

--- Backend/src/controllers/executeCode.controller.js
+++ Backend/src/controllers/executeCode.controller.js
@@ -1,3 +1,4 @@
+import { compile } from "tailwindcss";
 import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
 
 export const executeCode = async (req , res) => {
@@ -34,6 +35,31 @@ export const executeCode = async (req , res) => {
         const results = await pollBatchResults(tokens)
         console.log(results);
 
+
+        let allPassed = true;
+        const detailedResults = results.map((result , i) => {
+            const stdout = result.stdout?.trim();
+            const expected_output = expected_outputs[i]?.trim();
+            if(stdout === expected_output) passed = 1;
+            if(!passed) allPassed = false;
+
+            return {
+                testCase : i+1,
+                passed,
+                stdout,
+                expecte: expected_output,
+                stderr: result.stderr || null , 
+                compile_output: result.compile_output || null,
+                status: result.status.description,
+                memory:result.memory ? `${result.memory} KB` : undefined,
+                time:result.time ? `${result.time} s` : undefined
+            }
+
+
+            
+        })
+
+
         res.status(200).json({
             message: "Code Executed!"
         })
@@ -43,4 +69,6 @@ export const executeCode = async (req , res) => {
     } catch (error) {
         
     }
+
+
 }
